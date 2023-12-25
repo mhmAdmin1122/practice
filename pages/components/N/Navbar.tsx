385e887552b2fdceda312242a6a8ae95a8fb55f3
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-12-25 16:40:14 +0500

    few changes in the components

--- pages/components/N/Navbar.tsx
+++ pages/components/N/Navbar.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logobox">
-        <Image src={""} alt='logo'/>
+        <Image src={""} alt="logo" />
       </div>
       <div className="tabsBox">
         <Link href={"/"}>Home</Link>
         <Link href={"/"}>About</Link>
         <Link href={"/"}>Services</Link>
-        <Link href={"/"}>Projects</Link>
+        <Link href={"/"}>Profucts</Link>
         <Link href={"/"}>Tools</Link>
       </div>
-      <div className="functionalBtns">
-        
-      </div>
+      <Link href={"/"} className="functionalBtns">
+        Profile
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ pages/page/about/index.tsx
@@ -0,0 +1,12 @@
+import About from '@/pages/components/A/About'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  )
+}
+
+export default AboutPage
